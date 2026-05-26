677a9533dd5dd21bc0c204720a7a3876bef2535d
Duc Hoang <hoangduc180204@gmail.com>
2026-05-26 11:03:56 +0700

    Cap nhat bai 3 gio hang thanh toan

--- app/config/vnpay.php
+++ app/config/vnpay.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'tmn_code' => 'VNP_TMN_CODE_CUA_BAN',
-    'hash_secret' => 'VNP_HASH_SECRET_CUA_BAN',
+    'tmn_code' => 'PO0L87ZU',
+    'hash_secret' => 'K7RVO6Q8LCCWBWUUXHUN83XEHIF22NSH',
 
     'payment_url' => 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
     'return_url' => 'https://retirement-checks-plants-william.trycloudflare.com/Vnpay/returnUrl',
